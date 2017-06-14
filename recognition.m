20b46c5d5100923833f81023fa6be3a6aef07b1a
Katarzyna Jablonska <kaska.j.1993@o2.pl>
2017-06-14 12:35:56 +0200

    new code

--- recognition.m
+++ recognition.m
@@ -1,19 +1,28 @@
 mirwaitbar(0);  %wyrzuca progress bar
-file = miraudio({'male.wav', 'female.wav', 'male2.wav', 'female2.wav'}, 'Normal', 'Trim', 'Center', 'Label', 1:2); %normalizacja pliku, trim przycinq
+file = miraudio('Folder', 'Normal', 'Trim', 'Label', 1:2); %normalizacja pliku, trim przycinq
 %file = miraudio('Folder', 'Normal', 'Trim', 'Center');
-%na koncu i pocztku puste fragmenty
-
+%na koncu i pocztku puste fragment
+fcc = mirmfcc(file);
+chroma = mirchromagram(file);
+frames = mirframe(file);
+fr_spectrum = mirspectrum(frames, 'Min', 1);
 spectrum = mirspectrum(file, 'Min', 1);
+f = mirpeaks(mircepstrum(fr_spectrum, 'Freq', 'Min', 0.0025), 'Total', 1);
+%
 zerocross = mirzerocross(file);      
 rms = mirrms(file); %globalna energia sygnalu
-q25 = mirrolloff(file, 'Threshold', 0.25); %czestotiwosc - 25% powinno sie znalezc ponizej
+q25 = mirrolloff(spectrum, 'Threshold', 0.25); %czestotiwosc - 25% powinno sie znalezc ponizej
 %czestotliwosci
-q75 = mirrolloff(file, 'Threshold', 0.75);
+q25_f = mirmean(mirrolloff(fr_spectrum, 'Threshold', 0.25));
+q75 = mirrolloff(spectrum, 'Threshold', 0.75);
+q75_f = mirmean(mirrolloff(fr_spectrum, 'Threshold', 0.75));
+pitch = mirpitch(spectrum, 'Mono');
+pitch_fr = mirpitch(fr_spectrum, 'Mono');
 %qr = mirgetdata(q75) - mirgetdata(q25);
 lowenergy = mirlowenergy(file); %ile % synalu ma mniejsza energie niz srednia
-centroid = mircentroid(file); %wylicza spectra centroid, first moment
-spread = mirspread(file); %2nd moment
-skewness = mirskewness(file); %3rd moment
-kurtosis = mirkurtosis(file); %4th moment
-flatness = mirflatness(file); %sr geom/sr aryt
-mirexport('results.arff', zerocross, rms, q25, q75, lowenergy, centroid, spread, skewness, kurtosis, flatness);
+centroid = mircentroid(spectrum); %wylicza spectra centroid, first moment
+spread = mirspread(spectrum); %2nd moment
+skewness = mirskewness(spectrum); %3rd moment
+kurtosis = mirkurtosis(spectrum); %4th moment
+flatness = mirflatness(spectrum); %sr geom/sr aryt
+mirexport('results.arff', f, fcc, chroma, zerocross, rms, q25, q25_f, q75, q75_f, pitch, pitch_fr, lowenergy, centroid, spread, skewness, kurtosis, flatness);
